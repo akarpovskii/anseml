97844eedf0004412b4382a54d7bdd0cfc165655c
Andrew Karpovsky <21952395+akarpovskii@users.noreply.github.com>
2019-11-30 23:56:04 +0700

    Initial folder structure.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to anseml
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject anseml "0.1.0-SNAPSHOT"
+  :description "ANother S-Expressions Markup Language"
+  :license {:name "MIT License"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns anseml.core})
